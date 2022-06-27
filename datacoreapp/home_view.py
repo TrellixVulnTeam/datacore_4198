f00198ffda7a987c86929cad84b0934f695b6b2a
Gph spc <Gph spc@DESKTOP-8JIHCHQ>
2022-06-27 17:46:23 +0300

    redesign

--- datacoreapp/home_view.py
+++ datacoreapp/home_view.py
@@ -10,3 +10,9 @@ from datacoreapp import master_page_view
 class HomeView(master_page_view.MasterPageView):
     english_name = 'Home'
     template_name = 'home'
+
+    def before_render(self, context, request):
+        context['databases_count'] = models.Database.objects.count()
+        context['banks_count'] = models.Bank.objects.count()
+        context['relations_count'] = models.Relation.objects.count()
+        context['views_count'] = models.View.objects.count()
---
+++ datacoreapp/static/js/chart-pie-demo.js
@@ -0,0 +1,22 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+Chart.defaults.global.animation.duration = 2000;
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: 'pie',
+  data: {
+    labels: ["Blue", "Red", "Yellow", "Green"],
+    datasets: [{
+      data: [12.21, 15.58, 11.25, 8.32],
+      backgroundColor: ['#78caff', '#ff9696', '#ffbf66', '#68d7c5'],
+    }],
+  },
+  responsive:true,
+  options: {
+    animation: {
+        duration: 2000,
+    }
+  }
+});
